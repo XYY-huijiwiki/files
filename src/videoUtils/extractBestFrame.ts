6e32ab15a94132c701606c82f0927445e1d36d96
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-03-17 13:58:19 +0000

    feat: add new test item

--- src/videoUtils/extractBestFrame.ts
+++ src/videoUtils/extractBestFrame.ts
@@ -8,7 +8,7 @@ async function extractBestFrame(
   const BLACK_THRESHOLD = 32; // Pixel brightness threshold (0-255)
   const BLACK_PERCENTAGE = 90; // Percentage of dark pixels to consider frame black
 
-  let foundFrame: number = 0;
+  let foundFrame: number = 1;
   let isFirstCheck: boolean = true;
 
   // Phase 1: Find the first non-black frame using FFmpeg's blackframe filter
@@ -19,12 +19,11 @@ async function extractBestFrame(
         `blackframe=${BLACK_THRESHOLD}:${BLACK_PERCENTAGE},metadata=print:file=-`,
         "-f",
         "null", // Discard output
-        "-lossless",
-        "1",
       ])
       .output("NUL")
       .on("stderr", (stderrLine: string) => {
         // Parse FFmpeg output to find the first non-black frame
+        // console.log(stderrLine);
         const frameMatch = stderrLine.match(/frame:(\d+)/);
         const blackMatch = stderrLine.match(/pblack:(\d+)/);
         const last_keyframeMatch = stderrLine.match(/last_keyframe:(\d+)/);
@@ -34,15 +33,19 @@ async function extractBestFrame(
           const frameNumber = parseInt(frameMatch[1], 10);
           const blackPercentage = parseInt(blackMatch[1], 10);
 
-          // if the first frame is not black
-          if (isFirstCheck && last_keyframe !== 0) {
-            foundFrame = 0;
-            resolve();
-          } else if (isFirstCheck && last_keyframe === 0) {
+          if (isFirstCheck) {
             isFirstCheck = false;
-          } else if (blackPercentage < BLACK_PERCENTAGE) {
-            foundFrame = frameNumber;
-            resolve(); // Stop processing once the first valid frame is found
+            if (last_keyframe !== 0) {
+              foundFrame = 1;
+              resolve();
+            }
+          } else {
+            if (blackPercentage < BLACK_PERCENTAGE) {
+              foundFrame = frameNumber;
+              resolve();
+            } else {
+              foundFrame++;
+            }
           }
         }
       })
--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ generateThumbnail("./test/video_started_with_blackframe.mp4");
 generateThumbnail("./test/image.png");
 generateThumbnail("./test/video_no_blackframe.mp4");
 generateThumbnail("./test/喜羊羊小顽皮_4_pack_intro.mp4");
+generateThumbnail("./test/2018_中秋_懒羊羊.mp4");
+generateThumbnail("./test/2018_七夕_动态壁纸.mp4");
 generateThumbnail(
   "./test/11lGfTdoioKMqR1qodfCKgSPXXUtV5YJ7tkC5WOj8oZivsacOnf01.jpg"
 );
